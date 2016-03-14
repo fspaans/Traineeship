301abd389b4bac09e01d939eeec119638c0de5b6
Cesar Chretien <cesarchre10@gmail.com>
2016-03-14 16:48:23 +0100

    Opdracht 11 bezig.

---
+++ nl.youngcapital.eersteproject/src/nl/youngcapital/eersteproject/PriceOfProduct.java
@@ -0,0 +1,16 @@
+package nl.youngcapital.eersteproject;
+
+import java.util.HashMap;
+
+public class PriceOfProduct {
+
+	public static void main(String[] args) {
+		HashMap<Integer, Double> h = new HashMap<Integer, Double>();
+		
+		h.put(1, 2.95);
+		h.put(2, 4.99);
+		h.put(3, 5.49);
+		h.put(4, 7.80);
+		h.put(5, 8.85);
+	}
+}
