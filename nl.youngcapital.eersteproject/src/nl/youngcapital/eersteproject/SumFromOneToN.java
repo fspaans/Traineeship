4328b5397d451a02f7b1b76facead16a4a546856
Cesar Chretien <cesarchre10@gmail.com>
2016-03-15 12:26:12 +0100

    Opdracht 13 en 14 af.

---
+++ nl.youngcapital.eersteproject/src/nl/youngcapital/eersteproject/SumFromOneToN.java
@@ -0,0 +1,9 @@
+package nl.youngcapital.eersteproject;
+
+public class SumFromOneToN {
+
+	public static void main(String[] args) {
+		int n = 20;
+		System.out.println("De som van alle gehele getallen van 1 tot en met " + n + " is : " + n*(n+1)/2);
+	}
+}
---
+++ nl.youngcapital.eersteproject/src/nl/youngcapital/eersteproject/SumFromOnetoNOddOnly.java
@@ -0,0 +1,9 @@
+package nl.youngcapital.eersteproject;
+
+public class SumFromOnetoNOddOnly {
+
+	public static void main(String[] args) {
+		int n = 9;
+		System.out.println("De som van alle oneven gehele getallen van 1 tot en met " + n + " is : " + ((n+1)/2)*((n+1)/2));
+	}
+}
