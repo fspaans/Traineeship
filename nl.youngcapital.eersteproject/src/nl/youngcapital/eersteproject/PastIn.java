9354e662e01512cd4428572b49357f7935fed578
César Chrétien <Csar Chrtien>
2016-03-16 23:47:23 +0100

    Bonus opgave probeersel

--- nl.youngcapital.eersteproject/src/nl/youngcapital/eersteproject/PastIn.java
+++ nl.youngcapital.eersteproject/src/nl/youngcapital/eersteproject/PastIn.java
@@ -8,11 +8,26 @@ public class PastIn {
 		int i = 9;
 		long l = 4;
 		boolean b = true;
-		System.out.println(pastIn(new AantalBytes(l),new AantalBytes(c)));
+		boolean t = pastIn(l,c);
+		System.out.println(t);
 	}
 	
-	public static boolean pastIn(AantalBytes a, AantalBytes b){
-		return a.getSize() <= b.getSize();
+	public static boolean pastIn(Object a, Object b) throws Exception{
+		return aantalbytes(a.getClass().cast(a)) < aantalbytes(b.getClass().cast(b));
 	}
 	
+	public static int aantalbytes(Object o) throws Exception { throw new IllegalArgumentException("Objecten mogen niet."); }
+	
+	public static int aantalbytes(Boolean b) throws Exception { throw new IllegalArgumentException("Booleans zijn niet duidelijk."); }
+	
+	public static int aantalbytes(Byte b) { return 1; }
+	
+	public static int aantalbytes(Short s) { return 2; }
+	public static int aantalbytes(Character c) { return 2; }
+	
+	public static int aantalbytes(Integer i) { return 4; }
+	public static int aantalbytes(Float f) { return 4; }
+	
+	public static int aantalbytes(Long l) { return 8; }
+	public static int aantalbytes(Double d) { return 8; }
 }
