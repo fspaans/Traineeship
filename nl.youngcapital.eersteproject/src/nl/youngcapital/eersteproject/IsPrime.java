5ca8c97ad93746584fa0995587607fb3e9f1b5cc
Cesar Chretien <cesarchre10@gmail.com>
2016-03-16 09:29:34 +0100

    Opdracht 17 af.

---
+++ nl.youngcapital.eersteproject/src/nl/youngcapital/eersteproject/IsPrime.java
@@ -0,0 +1,23 @@
+package nl.youngcapital.eersteproject;
+
+
+
+public class IsPrime {
+
+	public static void main(String[] args) {
+
+		int[] nums = {127,327,523};
+		for(int i = 0; i < nums.length; i++) {
+			int check = (int) Math.sqrt(nums[i]);
+			int j = 2;
+			for(j = 2; j <= check; j++) {
+				if(nums[i] % j == 0) {
+					System.out.println(nums[i] + " is geen priemgetal");
+					break;
+				}
+			}
+			if (j == check+1)
+				System.out.println(nums[i] + " is een priemgetal");
+		}
+	}
+}
--- nl.youngcapital.eersteproject/src/nl/youngcapital/eersteproject/SubsequentNumbers.java
+++ nl.youngcapital.eersteproject/src/nl/youngcapital/eersteproject/SubsequentNumbers.java
@@ -3,14 +3,13 @@ package nl.youngcapital.eersteproject;
 public class SubsequentNumbers {
 	public static void main (String[] args) {
 
-		int[] seq = {5, 9, 13, 17, 21};
-		int[] nextseq = new int[10];
-		nextseq[0] = 5;
-		for(int i = 1; i < nextseq.length; i++)
-			nextseq[i] = nextseq[i - 1] + 4;
+		int[] seq = new int[10];
+		seq[0] = 5;
+		for(int i = 1; i < seq.length; i++)
+			seq[i] = seq[i - 1] + 4;
 		System.out.print("De reeks van tien getallen is ");
-		for(int i = 0; i < nextseq.length - 1; i++)
-			System.out.print(nextseq[i] + ", ");
-		System.out.println(nextseq[nextseq.length - 1] + ".");
+		for(int i = 0; i < seq.length - 1; i++)
+			System.out.print(seq[i] + ", ");
+		System.out.println(seq[seq.length - 1] + ".");
 	}
 }
