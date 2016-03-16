c202b0ef1bb89a0c7254bf48daa1653109b101b0
Cesar Chretien <cesarchre10@gmail.com>
2016-03-16 13:16:58 +0100

    Opdracht 20 af.

---
+++ nl.youngcapital.eersteproject/src/nl/youngcapital/eersteproject/ValueOfLetter.java
@@ -0,0 +1,14 @@
+package nl.youngcapital.eersteproject;
+
+import java.util.Scanner;
+
+public class ValueOfLetter {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Voer een karakter in:");
+		char c = s.next().charAt(0);
+		int val = c - 64;
+		val = (val > 26) ? val - 32 : val;
+		System.out.println("De waarde van " + c + " is " + val + ".");
+	}
+}
