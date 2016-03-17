399bf34241d4fb429929a19378a128651064d7a0
César Chrétien <Csar Chrtien>
2016-03-17 07:40:42 +0100

    Bonus gefixt.

--- nl.youngcapital.eersteproject/src/nl/youngcapital/eersteproject/PastIn.java
+++ nl.youngcapital.eersteproject/src/nl/youngcapital/eersteproject/PastIn.java
@@ -8,26 +8,32 @@ public class PastIn {
 		int i = 9;
 		long l = 4;
 		boolean b = true;
-		boolean t = pastIn(l,c);
+		boolean t = pastIn(c,l);
 		System.out.println(t);
 	}
 	
 	public static boolean pastIn(Object a, Object b) throws Exception{
-		return aantalbytes(a.getClass().cast(a)) < aantalbytes(b.getClass().cast(b));
+		return aantalbytes(a.getClass().cast(a)) <= aantalbytes(b.getClass().cast(b));
 	}
 	
-	public static int aantalbytes(Object o) throws Exception { throw new IllegalArgumentException("Objecten mogen niet."); }
-	
-	public static int aantalbytes(Boolean b) throws Exception { throw new IllegalArgumentException("Booleans zijn niet duidelijk."); }
-	
-	public static int aantalbytes(Byte b) { return 1; }
-	
-	public static int aantalbytes(Short s) { return 2; }
-	public static int aantalbytes(Character c) { return 2; }
-	
-	public static int aantalbytes(Integer i) { return 4; }
-	public static int aantalbytes(Float f) { return 4; }
-	
-	public static int aantalbytes(Long l) { return 8; }
-	public static int aantalbytes(Double d) { return 8; }
+	public static int aantalbytes(Object o) throws Exception {
+		if (o instanceof Byte) {
+			return 1;
+		}
+		else if (o instanceof Character || o instanceof Short) {
+			return 2;
+		}
+		else if (o instanceof Integer || o instanceof Float) {
+			return 4;
+		}
+		else if (o instanceof Long || o instanceof Double) {
+			return 8;
+		}
+		else if (o instanceof Boolean) {
+			throw new IllegalArgumentException("Booleans zijn niet duidelijk.");
+		}
+		else {
+			throw new IllegalArgumentException("Objecten mogen niet."); 
+		}
+	}
 }
