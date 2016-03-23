f37795638cea4d4c5cf718b0d4acfa390904f728
fspaans <fspaans86@gmail.com>
2016-03-23 16:08:30 +0100

    no message

---
+++ src/nl/fs/demo/Driehoek.java
@@ -0,0 +1,14 @@
+package nl.fs.demo;
+
+public class Driehoek{
+	
+	public static void main(String[] args){
+		int vv[][] = new int[11][11];
+		for(int i =1; i<11;i++){
+			for(int j =1; j< 11;j++){
+				vv[i][j] = i*j;
+				System.out.println(vv[i][j]);
+			}
+		}
+	}
+}
