eb1fbaad71e4cf0ddb521274dc1022ae4997e18c
fspaans <fspaans86@gmail.com>
2016-03-24 16:50:54 +0100

    tba

---
+++ src/nl/fs/tba/Square.java
@@ -0,0 +1,20 @@
+package nl.fs.tba;
+
+public abstract class Square{
+	public String descr= " ";
+	public String getDescription() {
+		return null;
+	}
+	
+	public Square(){
+		
+		
+		
+	}
+	public Square(String description){
+		
+		
+		
+	}
+	
+}
---
+++ src/nl/fs/tba/Wall.java
@@ -0,0 +1,7 @@
+package nl.fs.tba;
+
+public class Wall extends Square{
+	
+	
+	public Square(descr){}
+}
